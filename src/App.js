5803fba173dfdc2dd344a8b68b62a3d72ef593dc
Aryan <arsharyanyadav@gmail.com>
2025-02-15 17:26:01 +0530

    Learning useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,27 +1,61 @@
 
 import './App.css';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect, useReducer } from 'react';
 // import BlogCart from './blog-cart';
+
+function blogsReducer(state, action){
+  switch(action.type){
+    case "ADD":
+      return [action.blog, ...state];
+    case "REMOVE":
+      return state.filter((blog,index)=> index !== action.ind);
+    default:
+      return [];
+  }
+
+}
 function App() {
-  const [allContent, setAllContent] =  useState([]);
+  //const [allContent, setAllContent] =  useState([]);
   const [formData, setformData] = useState({title:"", content:""});
+  const [allContent, dispatch] = useReducer(blogsReducer, [])
   const titleRef = useRef(null);
   const handleRemove =(index)=>{
    
-   setAllContent(allContent.filter((blog,ind)=>ind !== index));
-   
+   //setAllContent(allContent.filter((blog,ind)=>ind !== index));
+   dispatch({type:"REMOVE",ind: index});
   }
+  useEffect(()=>{
+    //When our page renders first time, work as mounting phase
+    titleRef.current.focus();
+  },[]);
+
+  //When we click Post button , new blog will get added to allContent array, means whenever this array get changed we want to show top most
+  //  blog title as a tab name
+  useEffect(()=>{
+    if(allContent.length){
+      document.title = allContent[allContent.length -1].title;
+    }
+    else{
+      document.title = "No Blogs !!"
+    }
+  },[allContent]);
+
+
+
   const handleSubmit=(e)=>{
     e.preventDefault();
     const newBlog = {
       title : formData.title,
       content : formData.content
     }
-    setAllContent([...allContent,newBlog]);
-
+    //setAllContent([...allContent,newBlog]);
+    dispatch({type: "ADD",blog:newBlog});
     setformData({title:"",content:""});
     titleRef.current.focus();
   }
+
+
+
   return (
     <>
       <div className="blog-form">
